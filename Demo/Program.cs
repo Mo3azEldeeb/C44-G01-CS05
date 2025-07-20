8adc5e9a2a1ab8df9942a0a9b7e944ee213f1b55
moaz eldeeb <moazldeeb@gmail.com>
2025-07-20 21:40:03 +0300

    V1{1D ,Array}

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Demo
+{
+    class programe
+    {
+        static void Main(string[] args)
+        {
+            #region 1D Array
+            //int[] num = { 1, 2, 3 };
+
+            //num = new int[3];
+            //Console.WriteLine($"size of array{num.Length},Dimension Array={num.Rank}");
+            //for ( int i = 0; i < num.Length; i++ ) 
+            //    Console.WriteLine( num[i] );
+            //foreach ( int item in num )
+            //    Console.WriteLine( item );
+            #region creation
+            //int [] num01 = { 1, 2, 3 };
+            //int[] num03 = new int[3];
+            //int[ ] num04 = {1,2,3 };    
+            //   int[] num05 =  [1,2,3];
+            #endregion
+            #endregion
+
+        }
+    }
+}
