362d93a50b67140bc926eb88d4adfc4f3a20feac
moaz eldeeb <moazldeeb@gmail.com>
2025-07-21 06:25:26 +0300

    Q1 and Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+
+namespace Assignment
+{
+    class Programe
+    {
+        static void Main(String[] args)
+        {
+            ///Arrays
+            #region Q1 
+            ////Write a program in C# Sharp to find the sum of all elements of the array.
+
+            //Console.Write("Enter the Number of digits: ");
+            //bool IsParsed = int.TryParse(Console.ReadLine(), out int n);
+
+            //int[] arr = new int[n];
+            //int sum = 0;
+
+            //Console.WriteLine("Enter the Numbers:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"number {i+1}: ");
+            //     bool IsParsed02 = int.TryParse(Console.ReadLine(), out arr[i]);
+            
+            //    sum += arr[i];
+            //}
+
+            //Console.WriteLine($"The sum of all numbers  is: {sum}");
+            #endregion
+            #region Q2
+            //// Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+            //Console.Write("Enter the size of the arrays: ");
+            //bool IsParsed0 = int.TryParse(Console.ReadLine(), out int size);
+
+            //int[] array1 = new int[size];
+            //int[] array2 = new int[size];
+
+            //Console.WriteLine("Enter elements for the first array:");
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.Write($"Element[{i}] of array1: ");
+            //    bool IsParsed01 = int.TryParse(Console.ReadLine(), out array1[i]);
+            //}
+
+            //Console.WriteLine("Enter elements for the second array:");
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.Write($"Element[{i}] of array2: ");
+            //    bool IsParsed02 = int.TryParse(Console.ReadLine(), out array2[i]);
+            //}
+
+
+            //int[] mergedArray = new int[size * 2];
+            //for (int i = 0; i < size; i++)
+            //{
+            //    mergedArray[i] = array1[i];
+            //    mergedArray[i + size] = array2[i];
+            //}
+
+
+            //Array.Sort(mergedArray);
+
+            //Console.WriteLine("Merged array in ascending order:");
+            //foreach (int num in mergedArray)
+            //{
+            //    Console.Write(num + " ");
+            //}
+
+            //Console.ReadKey();
+            #endregion
+        }
+    }
+}
